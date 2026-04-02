2b5cef8fe2e24e7823d1ea3f899ec01a83bd0810
HolyLordPotato <81551645+HolyLordPotato@users.noreply.github.com>
2026-04-02 21:55:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 
 // Use localhost for web/iOS and Android emulator host mapping for Android.
 export const API_URL = Platform.OS === 'android'
-  ? 'http://10.0.2.2:3000'
-  : 'http://localhost:3000';
+  ? 'http://10.0.2.2:5432'
+  : 'http://localhost:5432';
 
   
